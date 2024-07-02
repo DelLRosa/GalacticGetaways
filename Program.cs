3a9308f8135d6aec43e0d002cd6a04882471ae6b
Delaney Green <delaney.rosa1@gmail.com>
2024-07-02 14:35:31 -0500

    database config

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using GalacticGetaways.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+   var connectionString = "server=localhost;user=galacticgetaways;password=galacticgetaways;database=galactic-getaways";
+   var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
